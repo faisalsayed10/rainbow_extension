963b128b2fd94e84801020b19c9e2032246a1f23
faisalsayed10 <faisal.sayed502@gmail.com>
2020-09-12 18:32:21 +0530

    master

--- popup.js
+++ popup.js
@@ -1,13 +1,16 @@
 replaceText(document.body);
+alert("Hello and Welcome! btw Hack Club is Kool! ")
 
 function replaceText(element) {
+
+
   if (element.hasChildNodes()) {
     element.childNodes.forEach(replaceText);
   } else if (element.nodeType === Text.TEXT_NODE) {
-    if (element.textContent.match(/coronavirus/gi)) {
+    if (element.textContent.match(/Hack Club/gi)) {
       const newElem = document.createElement("span");
       newElem.innerHTML = element.textContent.replace(
-        /(coronavirus)/gi,
+        /(Hack Club)/gi,
         '<span class="rainbow">$1</span>'
       );
       element.replaceWith(newElem);
